be0b9c67fcf2049db5770a74622bd566ce7e07cf
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-09-12 14:28:33 -0600

    add request test for creating a new subscription, add setup

---
+++ spec/requests/api/v1/subscriptions_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Subscriptions", type: :request do
+  describe "Subscription API" do
+    let!(:customer) {create(:customer)}
+    let!(:tea) {create(:tea)}
+    
+    describe "POST /api/v1/subscriptions" do
+      it "creates a new subscription" do
+        attrs = {
+          title: "Earl Grey",
+          price: 5.00,
+          status: "active",
+          frequency: "monthly",
+          customer_id: customer.id,
+          tea_id: tea.id
+        }
+
+        post "/api/v1/subscriptions", params: attrs
+        sub_data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+end
