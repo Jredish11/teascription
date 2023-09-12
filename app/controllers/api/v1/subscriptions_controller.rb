5fba837ee9f7b648939e1fae2d17575ad08d7928
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-09-12 16:21:11 -0600

    fix create action to take in conditional for if customer or tea doesn't exist

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def create
-    subscription = Subscription.create!(subscription_params)
-    if subscription.save
-      render json: SubscriptionSerializer.new(subscription), status: 200
+    if subscription_params[:customer_id].present? && subscription_params[:tea_id].present?
+      subscription = Subscription.create!(subscription_params)
+      if subscription.save
+        render json: SubscriptionSerializer.new(subscription), status: 200
+      else
+        render json: { error: subscription.errors.messages }, status: 422
+      end
     else
-      render json: { error: subscription.errors.messages }, status: 422
+      render json: { error: "Customer must exist" }, status: 422
     end
   end
 
