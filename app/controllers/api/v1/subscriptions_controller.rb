07854ecb8bee086f31a9358c9a3e0568148da0ae
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-09-12 14:42:53 -0600

    update create action, make private method for sub_params

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def create
-  
+    subscription = Subscription.create!(subscription_params)
+    if subscription.save
+      render json: SubscriptionSerializer.new(subscription), status: 200
+    else
+      render json: { error: subscription.errors.messages }, status: 422
+    end
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:title, :price, :status, :frequency, :customer_id, :tea_id)
   end
 end
