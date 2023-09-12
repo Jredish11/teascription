402410d92a110b6d9b2bc9be23fe35512a321bde
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-09-12 11:25:07 -0600

    fix sub table, add reference key for tea to create many to many relationship between customer and tea through subscription

--- db/schema.rb
+++ db/schema.rb
@@ -29,9 +29,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_170223) do
     t.string "status"
     t.integer "frequency"
     t.bigint "customer_id", null: false
+    t.bigint "tea_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+    t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
   end
 
   create_table "teas", force: :cascade do |t|
@@ -44,4 +46,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_170223) do
   end
 
   add_foreign_key "subscriptions", "customers"
+  add_foreign_key "subscriptions", "teas"
 end
