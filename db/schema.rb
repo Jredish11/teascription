1838e63854bf1754df25ad88db3ba0b1afdddb66
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-09-13 14:42:56 -0600

    delete unnecessary status column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_000701) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_13_204050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_000701) do
   create_table "subscriptions", force: :cascade do |t|
     t.string "title"
     t.float "price"
-    t.string "status"
     t.string "frequency"
     t.bigint "customer_id", null: false
     t.bigint "tea_id", null: false
